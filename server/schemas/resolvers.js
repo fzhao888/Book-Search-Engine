90b967e0b09f16736243d88b31cf0b6df1e3132d
fzhao888 <frank.zhao93@gmail.com>
2023-10-04 14:34:18 -0500

    Added resolvers, implemented typeDefs and utils/queries.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+// import user model
+const { User } = require('../models');
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        
+    },
+    Mutation: {
+
+    }
+
+};
+
+module.exports = resolvers;
