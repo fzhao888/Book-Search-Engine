951df1d4c0968587a12dacf8520000d97571fc86
fzhao888 <frank.zhao93@gmail.com>
2023-10-04 15:09:02 -0500

    Added GET_ME query and REMOVE_BOOK to SearchBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,41 +6,28 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+ 
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ME } from '..utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 
-const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+
+const SavedBooks = () => { 
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // creates a useQuery to execute get me query
+  const {loading, data} = useQuery(GET_ME);
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+  // loads and saves the query to userData
+  const userData = data?.me || [];
+  
+  // useMutation hook to remove a book
+  const [removeBook, {error}] = useMutation(REMOVE_BOOK);
+  
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -51,13 +38,10 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      const {data} = await removeBook(bookId);
+ 
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
+      const updatedUser = await data.json();
       setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -12,6 +12,7 @@ import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -77,13 +78,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({
+      const {data} = await saveBook({
         variables: {bookData: {...bookToSave} }
-      });
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      }); 
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
