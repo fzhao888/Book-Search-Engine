66c0b281e03d264023e3b261ebf8b34dd6b7ac3c
fzhao888 <frank.zhao93@gmail.com>
2023-10-04 19:14:55 -0500

    Edited server.js to implement apollo server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,19 @@
 // import user model
-const { User } = require('../models');
+const { User, Book } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
-    },
-    Mutation: {
+        me: async (parent, args) => {
+            return await User.findOne({
+                $or: [{ _id: user ? user._id : args.id }, { username: args.username }]
+            });
+        },
+        Mutation: {
 
+        }
     }
-
 };
 
 module.exports = resolvers;
